43bc2fba7376b71f668de57500ca825b1ff9c575
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2018-10-23 19:12:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hi");
+const left=document.getElementById("left");
+const right=document.getElementById("right");
+const full=document.getElementById("container");
+
+
+left.addEventListener('mouseenter',() =>{
+container.classList.add('hover-left');}
+);
+
+left.addEventListener('mouseleave',() =>{
+container.classList.remove('hover-left');}
+);
+
+right.addEventListener('mouseenter',() =>{
+container.classList.add('hover-right');}
+);
+
+right.addEventListener('mouseleave',() =>{
+container.classList.remove('hover-right');}
+);
+console.log("hi");
